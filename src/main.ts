73864a10027ea6d5c986b89cd4dc017787c8aa79
Clara Battesini <clarabattesini@gmail.com>
2023-12-26 21:14:59 -0500

    chore: creating the project again using vite

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import './assets/main.css'
 
-createApp(App).use(store).use(router).mount("#app");
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
