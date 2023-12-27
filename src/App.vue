04ced7f7c3d08696df4506cd206ce8308aecf85b
Clara Battesini <clarabattesini@gmail.com>
2023-12-27 16:37:47 -0500

    fix: page responsivity

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
-    <Sidebar />
-    <div class="right-panel">
-      <Menubar />
-      <main>
-        <RouterView />
-        Content will be rendered here
-      </main>
-    </div>
+  <Sidebar />
+  <div class="main-panel">
+    <Menubar />
+    <main>
+      <RouterView />
+      Content will be rendered here
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Sidebar from './components/Sidebar.vue';
-import Menubar from './components/Menubar.vue';
+import { defineComponent } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import Menubar from './components/Menubar.vue'
 // import { RouterView } from 'vue-router'
 
 export default defineComponent({
   name: 'App',
   components: { Sidebar, Menubar }
-});
-
+})
 </script>
 
 <style>
-
-.right-panel {
-  width: 79.5%;
+.main-panel {
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
