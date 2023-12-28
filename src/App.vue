9d19dd5a52cbff1c75e7429481e2055971499cbe
Clara Battesini <clarabattesini@gmail.com>
2023-12-27 20:43:56 -0500

    feat: setting up routing feature

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
   <div class="main-panel">
     <Menubar />
     <main>
-      <RouterView />
-      Content will be rendered here
+        <RouterView />
     </main>
   </div>
 </template>
@@ -13,11 +12,11 @@
 import { defineComponent } from 'vue'
 import Sidebar from './components/Sidebar.vue'
 import Menubar from './components/Menubar.vue'
-// import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 export default defineComponent({
   name: 'App',
-  components: { Sidebar, Menubar }
+  components: { Sidebar, Menubar, RouterView }
 })
 </script>
 
@@ -31,12 +30,12 @@ export default defineComponent({
 }
 
 main {
-  width: 95%;
+  width: 80%;
   height: 90%;
   box-sizing: border-box;
   margin: 0 2rem;
   padding: 2rem;
-  background-color: #f5f5f5;
+  background-color: #fff;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
 }
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    HOME
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+name: 'HomeView'
+})
+</script>
