8c0aae7e03829bc08d5176f5526cab15db56c992
Clara Battesini <clarabattesini@gmail.com>
2023-12-29 18:26:03 -0500

    feat: getting pets from the user

--- src/views/FoodAdvice/FoodAdvice.vue
+++ src/views/FoodAdvice/FoodAdvice.vue
@@ -8,18 +8,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent, reactive, ref, computed } from 'vue'
+import { Pet } from '@/types'
+import { useUserStore } from '@/stores/userStore'
 import Step1 from './Step1.vue'
 import Step2 from './Step2.vue'
 import Step3 from './Step3.vue'
 
 export default defineComponent({
-  name: 'DogFoodAdviceView',
+  name: 'FoodAdviceView',
   setup() {
+    const userStore = useUserStore()
     const totalSteps = ref<Number>(3)
+
     const formState = reactive({
-      currentStep: 1
+      currentStep: 1,
+      availablePets: computed(() => userStore.getPetsList),
+      chosedPet: null,
+      chosedConditions: []
     })
+
+    const setPet = (pet: Pet) => {
+      formState.chosedPet = pet
+    }
 
     const goToNextStep = () => {
       if (formState.currentStep < totalSteps.value) {
@@ -33,7 +44,7 @@ export default defineComponent({
       }
     }
 
-    return { formState, goToNextStep, goToPreviousStep, totalSteps }
+    return { formState, goToNextStep, goToPreviousStep, totalSteps, setPet }
   },
   components: {
     Step1,
