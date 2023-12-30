317dbdf3de32560de259f76bdcfb5a95b2c2bf09
Clara Battesini <clarabattesini@gmail.com>
2023-12-29 23:42:55 -0500

    feat: Add PickingPet component and styles

---
+++ src/components/PickingPet.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="token">
+    <div class="pet-image">
+      <img v-if="pictureUrl" :src="pictureUrl" alt="pet picture" />
+      <img
+        v-else-if="species === 'DOG'"
+        class="placeholder"
+        alt="pet icon placeholder"
+        :src="dogIcon"
+      />
+    </div>
+    <p class="pet-name">{{ name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import dogIcon from '@/assets/icons/dog.png'
+
+export default defineComponent({
+  name: 'PickingPetComponent',
+  components: {},
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    pictureUrl: {
+      type: String,
+      required: false
+    },
+    species: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dogIcon
+    }
+  }
+})
+</script>
+
+<style>
+.token {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem;
+  cursor: pointer;
+}
+
+.pet-image {
+  display: flex;
+  justify-content: center;
+  width: 6rem;
+  height: 6rem;
+  border-radius: 100%;
+  overflow: hidden;
+  background-color: #f3f3f3;
+  transition: all 0.1s ease-in-out;
+}
+
+.pet-image:hover {
+  border-radius: 99999;
+  outline: solid 4px rgb(88 80 236);
+  transition: all 0.1s ease-in-out;
+}
+
+.pet-image > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.pet-image > .placeholder {
+  width: 3.5rem !important;
+  object-fit: contain;
+}
+
+.pet-name {
+  text-align: center;
+  margin-top: 0.5rem;
+  font-weight: 400;
+}
+</style>
--- src/views/FoodAdvice/Step1.vue
+++ src/views/FoodAdvice/Step1.vue
@@ -1,16 +1,27 @@
 <template>
   <h1>Who is the food for?</h1>
-  <p v-for="pet in pets" :key="pet.id">{{ pet.name }}</p>
+  <PickingPet v-for="pet in pets" :key="pet.id" :name="pet.name" :species="pet.species" />
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { Pet } from '@/types'
+import PickingPet from '@/components/PickingPet.vue'
 
 export default defineComponent({
   name: 'Step1View',
   props: {
     pets: Array as PropType<Pet[]>
+  },
+  components: {
+    PickingPet
   }
 })
 </script>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+  margin-top: 6rem;
+}
+</style>
