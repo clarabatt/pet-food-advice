a2c038398dbd237c75192cb8ebe684bf41479db3
Clara Battesini <clarabattesini@gmail.com>
2023-12-29 23:02:31 -0500

    feat: passing pets as props to Step1 component

--- src/views/FoodAdvice/Step1.vue
+++ src/views/FoodAdvice/Step1.vue
@@ -1,11 +1,16 @@
 <template>
-    Step 1
+  <h1>Who is the food for?</h1>
+  <p v-for="pet in pets" :key="pet.id">{{ pet.name }}</p>
 </template>
-  
+
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import { Pet } from '@/types'
 
 export default defineComponent({
-name: 'Step1View'
+  name: 'Step1View',
+  props: {
+    pets: Array as PropType<Pet[]>
+  }
 })
 </script>
