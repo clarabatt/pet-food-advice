e037d63aa16fb8a2060185307d46153da2723f95
Clara Battesini <clarabattesini@gmail.com>
2023-12-30 23:40:56 -0500

    feat: condition token structure and using it on step2 view

---
+++ src/components/ConditionToken.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="token">
+    <div class="condition-icon">
+      <!-- <img v-if="pictureUrl" :src="pictureUrl" alt="pet picture" />
+      <img
+        v-else-if="species === 'DOG'"
+        class="placeholder"
+        alt="pet icon placeholder"
+        :src="dogIcon"
+      /> -->
+    </div>
+    <p class="condition-name">{{ name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Icon } from '@/types'
+import dogIcon from '@/assets/icons/dog.png'
+
+export default defineComponent({
+  name: 'ConditionTokenComponent',
+  components: {},
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    iconUrl: {
+      type: String,
+      required: false
+    },
+    icon: {
+      type: Object as Icon,
+      required: false
+    }
+  },
+  data() {
+    return {
+      dogIcon
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.token {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem 0.5rem;
+  height: 6rem;
+  width: 11rem;
+  cursor: pointer;
+  border: rgb(205 219 254) solid 1px;
+  border-radius: 0.5rem;
+  transition: all 0.15s ease-in-out;
+}
+
+.token:hover {
+  outline: solid 2px rgb(104 117 245);
+  transition: all 0.15s ease-in-out;
+}
+
+.condition-image > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.condition-image > .placeholder {
+  width: 3.5rem !important;
+  object-fit: contain;
+}
+
+.condition-name {
+  text-align: center;
+  margin-top: 0.5rem;
+  font-weight: 300;
+  color: black;
+}
+</style>
--- src/views/FoodAdvice/Step2.vue
+++ src/views/FoodAdvice/Step2.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Does {{ petName }} have any of those conditions?</h1>
+  <div class="conditions-step">
+    <h1>Does {{ petName }} have any of those conditions?</h1>
+    <p>Choose all that apply:</p>
+    <div class="conditions">
+      <ConditionToken v-for="condition in conditions" :key="condition" :name="condition" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import ConditionToken from '@/components/ConditionToken.vue'
 
 export default defineComponent({
   name: 'Step2View',
@@ -13,20 +20,46 @@ export default defineComponent({
       required: true
     }
   },
-  components: {}
+  setup() {
+    const conditions = ref<String[]>([
+      'Allergies or Food Sensitivities',
+      'Diabetes',
+      'Digestive issues',
+      'Dental issues',
+      'Mobility concerns',
+      'Overweight',
+      'Urinary problems',
+      'Heart Disease',
+      'Skin/Coat problems'
+    ])
+    return { conditions }
+  },
+  components: { ConditionToken }
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.conditions-step {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 6rem;
+}
+
 h1 {
   text-align: center;
   margin-top: 6rem;
+  width: 100%;
 }
-.pets {
+
+.conditions {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   margin-top: 2rem;
+  max-width: 66%;
 }
 </style>
