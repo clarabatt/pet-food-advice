bd8717dfa342e8ed477b973411a0c403c8796c81
Clara Battesini <clarabattesini@gmail.com>
2024-01-08 15:26:09 -0500

    feat: loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,81 @@
+<template>
+  <span class="loader"></span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoadingComponent'
+})
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  animation: rotate 1s infinite;
+  height: 50px;
+  width: 50px;
+}
+
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  content: '';
+  display: block;
+  height: 20px;
+  width: 20px;
+}
+.loader:before {
+  animation: ball1 1s infinite;
+  background-color: #ec4899;
+  box-shadow: 30px 0 0 #423898;
+  margin-bottom: 10px;
+}
+.loader:after {
+  animation: ball2 1s infinite;
+  background-color: #423898;
+  box-shadow: 30px 0 0 #ec4899;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg) scale(0.8);
+  }
+  50% {
+    transform: rotate(360deg) scale(1.2);
+  }
+  100% {
+    transform: rotate(720deg) scale(0.8);
+  }
+}
+
+@keyframes ball1 {
+  0% {
+    box-shadow: 30px 0 0 #423898;
+  }
+  50% {
+    box-shadow: 0 0 0 #423898;
+    margin-bottom: 0;
+    transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #423898;
+    margin-bottom: 10px;
+  }
+}
+
+@keyframes ball2 {
+  0% {
+    box-shadow: 30px 0 0 #ec4899;
+  }
+  50% {
+    box-shadow: 0 0 0 #ec4899;
+    margin-top: -20px;
+    transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #ec4899;
+    margin-top: 0;
+  }
+}
+</style>
