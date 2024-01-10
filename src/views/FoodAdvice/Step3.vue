c1bcc13d4867bb7fc5c87bfe440c3537fd7bffe2
Clara Battesini <clarabattesini@gmail.com>
2024-01-09 20:35:47 -0500

    feat: using PetFoodSubscription component on step3

--- src/views/FoodAdvice/Step3.vue
+++ src/views/FoodAdvice/Step3.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-for="food in foodList" :key="food._id">
-    {{ food.name }}
-    {{ food.price }}
+  <div class="foodRecommendations">
+    <h1>The recommended options for {{ petName ? petName : 'your pet' }} are:</h1>
+    <PetFoodSubscription v-for="food in foodList" :key="food._id" :food="food" />
   </div>
 </template>
 
@@ -9,9 +9,17 @@
 import { computed, defineComponent } from 'vue'
 import { useRecommendationStore } from '@/stores/recommendationStore'
 import type { RecommendationResponse } from '@/services/recommendationApi'
+import PetFoodSubscription from '@/components/PetFoodSubscription.vue'
 
 export default defineComponent({
   name: 'Step3View',
+  props: {
+    petName: {
+      type: String,
+      required: false
+    }
+  },
+  components: { PetFoodSubscription },
   setup() {
     const recommendationStore = useRecommendationStore()
 
@@ -26,3 +34,13 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.foodRecommendations {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0;
+}
+</style>
