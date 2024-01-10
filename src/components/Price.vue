a7ac242611c5b913632651c1c0ead545cb896362
Clara Battesini <clarabattesini@gmail.com>
2024-01-09 20:34:30 -0500

    feat: price component

---
+++ src/components/Price.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="priceComponent">
+    <p>$</p>
+    <p class="price">{{ price }}</p>
+    <p>/month</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PetFoodSubscriptionComponent',
+  props: {
+    price: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {}
+})
+</script>
+
+<style lang="scss" scoped>
+.priceComponent {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: center;
+  margin: 1.5rem 0;
+}
+
+.priceComponent > .price {
+  font-size: 2.3rem;
+  font-weight: 600;
+  margin: 0 0.5rem;
+  line-height: 0.8;
+}
+</style>
